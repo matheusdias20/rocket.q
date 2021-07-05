bb4944e0e5f169e32c84a6be02c2d9d6ba444747
Matheus Vieira <mvdias20@gmail.com>
2021-07-05 12:57:36 -0300

    Setting route

--- src/route.js
+++ src/route.js
@@ -1,5 +1,14 @@
-const express = require('express')
+const express = require("express");
+const QuestionController = require("./controllers/QuestionController");
 
-const route = express.Router()
+const route = express.Router();
 
-route.get('/', (req, res) => res.render("index"))
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-pass", (req, res) =>
+  res.render("create-pass", { page: "create-pass" })
+);
+route.get("/room", (req, res) => res.render("room"));
+
+route.post("/room/:room/:question/:action", QuestionController.index);
+
+module.exports = route;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
-const express = require('express')
+const express = require("express");
+const route = require("./route");
+const path = require("path");
 
-const server = express()
+const server = express();
 
-server.listen(3000, () => console.log("RODANDO"))
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "views"));
+
+server.use(express.static("public"));
+server.use(route);
+server.use(express.urlencoded({ extended: true }));
+
+server.listen(3000, () => console.log("RODANDO"));
