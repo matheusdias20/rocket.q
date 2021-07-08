a30374c17db031e6fc09ca976c40513a488e41e9
matheusdias20 <65796589+matheusdias20@users.noreply.github.com>
2021-07-07 23:18:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const path = require("path");
 
 const server = express();
 
+// Configuração da porta - Heroku
+var porta = process.env.PORT || 8080
+
 server.set("view engine", "ejs");
 
 server.use(express.static("public"));
@@ -14,8 +17,7 @@ server.use(express.urlencoded({ extended: true }));
 
 server.use(route);
 
-// Configuração da porta - Heroku
-var porta = process.env.PORT || 8080
+server.listen(porta, () => console.log('rodando'))
 
 // Configuração da porta - Local
 // server.listen(3000, () => console.log("RODANDO"));
